65a34262c2221104d5e43880b59fd13526c83f11
Stepan72 <st.nim@list.ru>
2023-06-06 14:16:10 +0600

    home data insert v0.03

--- app/page.js
+++ app/page.js
@@ -1,15 +1,28 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import FirmCard from "@/components/FirmCard";
 const data = [1, 2, 3, 4, 5, 6, 7];
+import { companies } from "@/data/companies";
 
 export default function Home() {
+  const [firmsData, setFirmsData] = useState(companies);
+
+  console.log(firmsData);
   return (
     <div className="flex flex-col">
       <header className="m-10 mt-20">
         <h1 className="text-center">Мои организации</h1>
       </header>
       <div id="container" className="mx-64 flex flex-wrap gap-2">
-        {data.map((el) => {
-          return <FirmCard />;
+        {firmsData.map((el, index) => {
+          return (
+            <FirmCard
+              key={el.company_id}
+              name={el.company_name}
+              innNumber={el.company_tin}
+              logo={el.logo}
+            />
+          );
         })}
       </div>
     </div>
--- components/FirmCard.jsx
+++ components/FirmCard.jsx
@@ -1,9 +1,8 @@
 "use client";
 import React from "react";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
-import { MdEditDocument } from "react-icons/md";
 
-function FirmCard() {
+function FirmCard({ name, innNumber, logo }) {
   return (
     <div
       className="h-20 m-1 flex flex-row justify-between border shadow-lg rounded-md hover:border-blue-500"
@@ -11,22 +10,25 @@ function FirmCard() {
     >
       <div className="flex flex-row">
         <div className="ml-5 w-20 p-2">
-          <img className="p-2" src="/icon.png" alt="firm-image" />
+          <img
+            className="p-2"
+            src={logo ? logo : "/building.png"}
+            alt="firm-image"
+          />
         </div>
         <div className="ml-4 w-56 my-3 gap-1 flex flex-col justify-center">
           <div>
-            <p className="text-sm text-gray-500">Naimenovanie</p>
+            <p className="text-sm text-gray-500">{name}</p>
           </div>
           <div className="flex flex-row gap-2">
-            <p className="text-sm text-gray-500">INN/BIN</p>
-            <p className="text-sm text-gray-500">Number</p>
+            <p className="text-sm text-gray-500">ИНН/БИН</p>
+            <p className="text-sm text-gray-500">{innNumber}</p>
           </div>
         </div>
       </div>
       <div className="mr-6 py-5 flex flex-row gap-3 items-center">
-        <FaEdit size={22} className="text-blue-400" />
-
-        <FaTrashAlt size={22} className="text-red-400" />
+        <FaEdit size={20} className="text-blue-400" />
+        <FaTrashAlt size={20} className="text-red-400" />
       </div>
     </div>
   );
